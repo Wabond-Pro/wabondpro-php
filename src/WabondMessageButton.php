3d2675168c066a9cd19677c19ad9e2a6c846b21d
ricardosawir <sawir.ricardo@gmail.com>
2021-09-13 21:25:53 +0700

    changed Type

--- src/WabondMessageButton.php
+++ src/WabondMessageButton.php
@@ -7,63 +7,17 @@ use WabondPro\WabondClient\Traits\Sendable;
 
 class WabondMessageButton implements Messagable
 {
-    use Sendable;
+  use Sendable;
 
-    protected $type = 'button';
+  protected $type = 'interactive';
 
-    protected $data = [
+  protected $data = [
     'contentText' => '',
     'footerText' => '',
     'headerType' => 1,
     'buttons' => [],
   ];
 
-<<<<<<< HEAD
-    public function __construct($secret)
-    {
-        $this->secret = $secret;
-    }
-
-    public function addButton($id, $text)
-    {
-        $this->data['buttons'][] = ['buttonId' => $id, 'buttonText' => ['displayText' => $text], 'type' => 1];
-
-        return $this;
-    }
-
-    public function setFooter($text)
-    {
-        $this->data['footerText'] = $text;
-
-        return $this;
-    }
-
-    public function setContent($text)
-    {
-        $this->data['contentText'] = $text;
-
-        return $this;
-    }
-
-    public function build()
-    {
-        return $this->data;
-    }
-
-    public function setHeaderType($type)
-    {
-        $this->data['headerType'] = $type;
-
-        return $this;
-    }
-
-    public function buildPayload()
-    {
-        return [
-      'data' => $this->data,
-    ];
-    }
-=======
   public function __construct($secret)
   {
     $this->secret = $secret;
@@ -102,5 +56,4 @@ class WabondMessageButton implements Messagable
   {
     return ['interactive' => $this->data];
   }
->>>>>>> 7be93c7 (normalizing request)
 }
--- src/WabondMessageText.php
+++ src/WabondMessageText.php
@@ -7,36 +7,6 @@ use WabondPro\WabondClient\Traits\Sendable;
 
 class WabondMessageText implements Messagable
 {
-<<<<<<< HEAD
-    use Sendable;
-    protected $message;
-    protected $withPreviewUrl = false;
-    protected $type = 'text';
-
-    public function __construct($secret)
-    {
-        $this->secret = $secret;
-    }
-
-    public function setBody($message)
-    {
-        $this->message = $message;
-
-        return $this;
-    }
-
-    public function withPreviewUrl()
-    {
-        $this->withPreviewUrl = true;
-
-        return $this;
-    }
-
-    public function buildPayload()
-    {
-        return ['data' => $this->message];
-    }
-=======
   use Sendable;
   protected $message;
   protected $withPreviewUrl = false;
@@ -64,5 +34,4 @@ class WabondMessageText implements Messagable
   {
     return ['text' => $this->message];
   }
->>>>>>> 7be93c7 (normalizing request)
 }
